d3b743e9b8091c6ecb5d68fa3941ee88ef67e06d
Bisrat Tamire <biarattamire288@gmail.com>
2024-11-30 22:53:58 +0300

    path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+import Home from "../src/pages/Home";
+import Register from "../src/pages/Register";
+import Login from "../src/pages/Login";
 
 function App() {
-  
   return (
-  <h1>
-    hello
-  </h1>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;

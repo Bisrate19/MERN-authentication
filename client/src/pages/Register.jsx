afafdfc5261b6a680cd996b58fbae4aefcde7cd6
Bisrat Tamire <biarattamire288@gmail.com>
2024-12-01 00:14:36 +0300

    1st login&register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import { useState } from "react";
+import React from "react";
 
 export default function Register() {
-  
-  const registerUser=(e)=>{
-    e.preventDefault()
-  }
-  
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const registerUser = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <form onSubmit={registerUser}>
         <label>Name</label>
-        <input type='text' placeholder='enter name...'/>
+        <input
+          type="text"
+          placeholder="enter name..."
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
         <label>Email</label>
-        <input type='email' placeholder='enter email...'/>
+        <input
+          type="email"
+          placeholder="enter email..."
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
 
         <label>Password</label>
-        <input type='password' placeholder='enter password...'/>
-<buttom type="submit">Submit</buttom>
+        <input
+          type="password"
+          placeholder="enter password..."
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }

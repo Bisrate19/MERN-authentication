57c0891bcb6746c482fcce525bcea8c859cca033
Bisrat Tamire <biarattamire288@gmail.com>
2024-11-30 23:14:37 +0300

    register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 export default function Register() {
+  
+  const registerUser=(e)=>{
+    e.preventDefault()
+  }
+  
   return (
-    <div>Register</div>
+    <div>
+      <form onSubmit={registerUser}>
+        <label>Name</label>
+        <input type='text' placeholder='enter name...'/>
+        <label>Email</label>
+        <input type='email' placeholder='enter email...'/>
+
+        <label>Password</label>
+        <input type='password' placeholder='enter password...'/>
+<buttom type="submit">Submit</buttom>
+      </form>
+    </div>
   )
 }

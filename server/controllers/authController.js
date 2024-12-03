9a83d4e0ad05fa1e0ba7196ed3a9909bd5642868
Bisrat Tamire <biarattamire288@gmail.com>
2024-12-04 00:08:23 +0300

    bcrypt working

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { hashPassword, comparePassword } = require("../helpers/auth");
 const test = (req, res) => {
   res.json("test is working");
 };
@@ -11,7 +12,7 @@ const registerUser = async (req, res) => {
     if (!name) {
       return res.json({
         error: "name is required",
-      }); 
+      });
     }
     //check if password is good
     if (!password || password.length < 6) {
@@ -27,10 +28,12 @@ const registerUser = async (req, res) => {
       });
     }
 
+    const hashedPassword = await hashPassword(password);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     return res.json(user);
   } catch (error) {
